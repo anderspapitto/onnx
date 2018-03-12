979c10ae889fb38bd458205772f4671476cc4e25
Anders Papitto <anderspapitto@gmail.com>
2018-03-12 11:00:44 -0700

    setup.py uses cmake build system

--- onnx/defs/__init__.py
+++ onnx/defs/__init__.py
@@ -3,8 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import onnx
-import onnx.onnx_cpp2py_export.defs as C
+import onnx_cpp2py_export.defs as C  # type: ignore
 
 
 ONNX_DOMAIN = ""
